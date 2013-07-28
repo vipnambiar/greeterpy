9331b215ac97e6d24cbe781bfb54c8d1ddfc7d6c
Vipin Nambiar <nambiar.vip@gmail.com>
2013-07-29 00:43:42 +0530

    adding greeter package

---
+++ greeter/greet.py
@@ -0,0 +1,116 @@
+#!/usr/local/bin/python2.7
+# encoding: utf-8
+'''
+greeter.greet - A simple greeter script
+greeter.greet is a simple greeter script which greets the script 
+executioner by the name he passes to the script as argument
+
+It defines a method greet which takes a name argument with default as "World"
+
+@author:     Vipin Nambiar
+        
+@copyright:  2013 Zeomega. All rights reserved.
+        
+@license:    Apache License 2.0
+
+@contact:    nambiar.vip@gmail.com
+@deffield    updated: Updated
+'''
+
+import sys
+import os
+
+from argparse import ArgumentParser
+from argparse import RawDescriptionHelpFormatter
+
+__all__ = []
+__version__ = 0.1
+__date__ = '2013-07-22'
+__updated__ = '2013-07-28'
+
+DEBUG = 0
+TESTRUN = 0
+PROFILE = 0
+
+class CLIError(Exception):
+    '''Generic exception to raise and log different fatal errors.'''
+    def __init__(self, msg):
+        super(CLIError).__init__(type(self))
+        self.msg = "E: %s" % msg
+    def __str__(self):
+        return self.msg
+    def __unicode__(self):
+        return self.msg
+
+def greet(name="World"):
+    print "Hello %s!" % name
+    return 0
+    
+def main(argv=None): # IGNORE:C0111
+    '''Command line options.'''
+    
+    if argv is None:
+        argv = sys.argv
+    else:
+        sys.argv.extend(argv)
+
+    program_name = os.path.basename(sys.argv[0])
+    program_version = "v%s" % __version__
+    program_build_date = str(__updated__)
+    program_version_message = '%%(prog)s %s (%s)' % (program_version, program_build_date)
+    program_shortdesc = __import__('__main__').__doc__.split("\n")[1]
+    program_license = '''%s
+
+  Created by Vipin Nambiar on %s.
+  Copyright 2013 organization_name. All rights reserved.
+  
+  Licensed under the Apache License 2.0
+  http://www.apache.org/licenses/LICENSE-2.0
+  
+  Distributed on an "AS IS" basis without warranties
+  or conditions of any kind, either express or implied.
+
+USAGE
+''' % (program_shortdesc, str(__date__))
+
+    try:
+        # Setup argument parser
+        parser = ArgumentParser(description=program_license, formatter_class=RawDescriptionHelpFormatter)
+        parser.add_argument('-V', '--version', action='version', version=program_version_message)
+        parser.add_argument("-n", '--name', dest="name", help="The name of the person to greet [default: %(default)s]", default="World")
+
+        
+        # Process arguments
+        args = parser.parse_args()
+        name  = args.name
+        return greet(name)
+    except KeyboardInterrupt:
+        ### handle keyboard interrupt ###
+        return 0
+    except Exception, e:
+        if DEBUG or TESTRUN:
+            raise e
+        indent = len(program_name) * " "
+        sys.stderr.write(program_name + ": " + repr(e) + "\n")
+        sys.stderr.write(indent + "  for help use --help\n")
+        return 2
+
+if __name__ == "__main__":
+    if DEBUG:
+        #sys.argv.append("-h")
+        sys.argv.append("-v")
+        sys.argv.append("-n Vipin")
+        import doctest
+        doctest.testmod()
+    if PROFILE:
+        import cProfile
+        import pstats
+        profile_filename = 'main.greet_profile.txt'
+        cProfile.run('main()', profile_filename)
+        statsfile = open("profile_stats.txt", "wb")
+        p = pstats.Stats(profile_filename, stream=statsfile)
+        stats = p.strip_dirs().sort_stats('cumulative')
+        stats.print_stats()
+        statsfile.close()
+        sys.exit(0)
+    sys.exit(main())
