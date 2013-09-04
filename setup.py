9be56513606993e16d1f865678037fcbca8b4b59
Vipin Nambiar <nambiar.vip@gmail.com>
2013-09-05 01:24:15 +0530

    renamed README.md to README.rst Modified entry point in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from distutils.core import setup
+from setuptools import setup
 
-with open('README.md') as file_descriptor:
+with open('README.rst') as file_descriptor:
     long_description = file_descriptor.read()
 
 setup(
@@ -9,8 +9,11 @@ setup(
     packages=['greeter',],
     license='Apache License 2.0',
     long_description=long_description,
+    package_data={ 
+                  '': ['README*', 'LICENSE*', '*.txt', '*.rst'], 
+                  },
     classifiers=[
         'Programming Language :: Python :: 2.7',
     ],
-    entry_points={ 'console_scripts' : ['greet = Greeter.greet:main',] }
+    entry_points={ 'console_scripts' : ['greet = greeter.greet:main',] },
 )
